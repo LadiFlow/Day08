4853f8abaf23405b8d1268f8df7bce0c73caf831
LadiFlow <adi_curt@hotmail.com>
2021-03-16 02:50:28 +0100

    Function: the parseInt()

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function convertSrtToInt(str) {
+    return parseInt(str);
+}
+console.log(convertSrtToInt("44"));
+console.log(convertSrtToInt("27"));
+console.log(convertSrtToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertSrtToInt;
